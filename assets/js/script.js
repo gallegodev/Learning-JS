068c3334f1a6955dbb31f33f9e94482cff4b9fd4
gallegodev <gallegodev1991@gmail.com>
2022-12-05 19:30:40 +0000

    Add var stats to JS structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,27 @@ var guess;
 var hits = 0;
 var numberOfGuesses = 0;
 var isSunk = false;
+var stats = "You took " + numberOfGuesses + " guesses to sink the battleship, " +
+"which means your shooting accuracy was " + (numberOfGuesses);
 
-while (isSunk == false){
-    guess = prompt("Ready, aim,fire! (enter anumber from 0-6):");
-    
-    if (guess < 0 ||  guess > 6){
-    alert("Player please enter a valid number");
+while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!");
     } else {
-        guess = guess + 1;
+        numberOfGuesses = numberOfGuesses + 1;
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+            alert("Hi!")
+        }else{
+            alert("Miss!")
+        }
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+        }
     }
 
-    if(guess = 3){
-        isSunk == true;
-        alert("You sank my batleship")
-    }
-}
+alert(stats);
